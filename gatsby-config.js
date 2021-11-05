2c235162982c5771e9abdbfb13005f2f8be91073
andreisemianiuk <semeniukandrei677@gmail.com>
2021-11-05 19:16:49 +0300

    Add source-wordpress plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,22 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+        {
+            /**
+             * First up is the WordPress source plugin that connects Gatsby
+             * to your WordPress site.
+             *
+             * visit the plugin docs to learn more
+             * https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-source-wordpress/README.md
+             *
+             */
+            resolve: `gatsby-source-wordpress`,
+            options: {
+                // the only required plugin option for WordPress is the GraphQL url.
+                url:
+                  process.env.WPGRAPHQL_URL ||
+                  `http://ecosiprom.com/graphql`,
+            },
+        },
     ]
 }
